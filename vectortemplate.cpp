c199ff17ebbf3b12b75aa487a502084aafd13613
Andrew Snow <asnow4u@gmail.com>
2019-10-02 11:04:15 -0700

    Couple more functions to go

--- vectortemplate.cpp
+++ vectortemplate.cpp
@@ -10,40 +10,55 @@
 #include "glm/gtc/matrix_transform.hpp"
 #include "glm/gtc/matrix_inverse.hpp"
 
+#include <iostream>
+
 glm::vec3 WhatPartOfALivesInBDir( glm::vec3 a, glm::vec3 b )
 {
-	???
+    //unit(B) * (A dot unit(b)
+    return normalize(b) * dot(a, normalize(b));
+
 }
 
 
 glm::vec3 WhatPartOfALivesPerpToB( glm::vec3 a, glm::vec3 b )
 {
-	???
+	//A - unit(B) * dot(A, unit(B)
+    return a - normalize(b) * dot(a, normalize(b));
 }
 
 
 glm::vec3 UnitSurfaceNormal( glm::vec3 q, glm::vec3 r, glm::vec3 s )
 {
-	 ???
+	 //n = (R-Q) X (S-Q)
+     return cross((r-q), (s-q));
 }
 
 
 float Area( glm::vec3 q, glm::vec3 r, glm::vec3 s )
 {
-	 ???
+	 //
 }
 
 
 bool IsPointInTriangle( glm::vec3 q, glm::vec3 r, glm::vec3 s, glm::vec3 p )
 {
-	???
+    glm::vec3 n = cross((r-q), (s-q));
+    glm::vec3 nq = cross((r-q), (p-q));
+    glm::vec3 nr = cross((s-r), (p-r));
+    glm::vec3 ns = cross((q-s), (p-s));
+
+    if (dot(n, nq) > 0 && dot(n, nr) > 0 && dot(n, ns)){
+        return true;
+    } else {
+        return false;
+    }
 }
 
 
 
 float DistanceFromPointToPlane( glm::vec3 q, glm::vec3 r, glm::vec3 s, glm::vec3 p )
 {
-	???
+	//???
 }
 
 
@@ -55,5 +70,5 @@ void WhoAmI( std::string &yourName, std::string &yourEmailAddress )
 
 //THIS IS FOR TESTING PURPOSES ONLY
 int main(){
-  print("HELLO WORLD");
+  std::cout << "HELLO WORLD";
 }
